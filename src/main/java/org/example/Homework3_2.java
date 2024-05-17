9df9278df80ce79c7ced239ada6863f272e170c0
Bakhtovar <btagayaliev@gmail.com>
2024-05-17 11:54:24 +0200

    Added files

---
+++ src/main/java/org/example/Homework3_2.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+/**
+ * Класс для реализации домашнего задания номер три, часть 2.
+ */
+public class Homework3_2 {
+
+    /**
+     * Реализовать метод при помощи {@link java.util.stream.Stream} который:
+     * - группирует заданых персон по возрасту
+     * - фильтрует по минимальному возрасту
+     * - Убирает дубликаты персон по имени
+     * @param persons - заданный список персон
+     * @param minimalAgeFilter - указанный минимальный возраст
+     * @param removeDupes - флаг отвечающий за исключение дубликатов из результата
+     * @return словарь где ключ хобби а значение лист персон с этим хобби
+     */
+    public Map<Integer, List<Person>> groupByAge(List<Person> persons, int minimalAgeFilter,  boolean removeDupes) {
+        return Map.of();
+    }
+}
